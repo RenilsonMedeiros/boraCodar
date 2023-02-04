4d21468456b3730da9926c65537a6ff6846a527b
Renilson Medeiros <renilsonsilva@aluno.uema.br>
2023-02-04 14:06:57 -0300

    Atualizações no design e no js v1.1

--- chat/script.js
+++ chat/script.js
@@ -2,6 +2,7 @@ const sendMessage = document.getElementById('send-message')
 const userMessage = document.getElementById('write-message')
 const main = document.querySelector('main')
 const personName = document.getElementById('username')
+const messages = document.querySelector('main')
 
 function getObjectName(object) {
   if(object == 'user') {
@@ -28,26 +29,28 @@ function addMessageInChat(object, objectMessage) {
   divMessage.appendChild(pInfo)
   divMessage.appendChild(pMessage)
   main.appendChild(divMessage)
+
+  messages.lastElementChild.scrollIntoView()
 }
 
 async function requestResponseAi() {
   if(userMessage.value.trim().length != 0) {
-    addMessageInChat('user', userMessage.value)
+    let userMessageValue = userMessage.value
+    userMessage.value = ''
 
-    let response = await fetch(`https://renilsonmedeiros.github.io/boraCodar/chat/chatapi.py/ai/${userMessage.value}`, {
+    addMessageInChat('user', userMessageValue)
+
+    let response = await fetch(`http://localhost:5000/ai/${userMessageValue}`, {
       method: 'GET',
       mode: 'cors',
     }).then(response => response.json()
     ).catch(err => {
       console.log(err)
-      userMessage.value = ''
       return null
     })
 
     addMessageInChat('person', response.ai)
   }
-
-  userMessage.value = ''
 }
 
 sendMessage.addEventListener("click", requestResponseAi)
